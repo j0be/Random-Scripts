a164ba1c9e4d18ccae58411c72631c2c410a3e90
Ronald Troyer <ron.troyer@gmail.com>
2023-06-17 15:14:39 -0400

    V2 MVP

--- resources/wordle-hint.js
+++ resources/wordle-hint.js
@@ -1,301 +1,155 @@
-window.hinter = {
-  state: {
-    ...window.hinter?.state,
-    game: 'wordle',
-    get: () => {
-      let domMapper = hinter.tiles.getDom();
-      return domMapper.map(row => ({
-        ...row,
-        ...(() => {
-          let statusId = hinter.rows.status(row);
-          return {
-            statusId,
-            status: hinter.constants.states.row[statusId],
-          };
-        })(),
-        tiles: row.tiles.map(tile => {
-          return {
-            ...tile,
-            ...(() => {
-              let statusId = hinter.tiles.status(tile);
-              return {
-                statusId,
-                status: hinter.constants.states.tile[hinter.tiles.status(tile)],
-              };
-            })(),
-            letter: hinter.tiles.letter(tile).toLowerCase(),
-          };
-        })
-      }));
-    },
-  },
-
-  constants: {
-    states: {
-      tile: ['empty', 'tbd', 'absent', 'present', 'correct'],
-      row: ['empty', 'pending', 'played']
-    }
-  },
-
-  init: () => {
+const [ CORRECT, PRESENT, ABSENT ] = [ 'correct', 'present', 'absent' ];
+  const init = () => {
+    const site = getSiteName();
     return Promise.all([
-      hinter.words.get(),
-      hinter.solution.get()
-    ]).then(([dict, solution]) => {
-      let rows = hinter.state.get();
-      hinter.words.hint(dict, rows);
-    })
-    .catch(err => {
-      debugger;
-      console.error('General Stack Error', err);
-      alert(`General Stack Error:\n${err.toString()}`);
+      getDictionary(site),
+      getBoard(site),
+      getSolution(site),
+    ]).then(([dictionary, board, solution]) => {
+      if (!dictionary) {
+        throw new Error('No dictionary found. Do we support this site?');
+      }
+
+      return new Promise((resolve) => {
+        const summary = getBoardSummary(board);
+        const regex = getRegex(summary);
+
+        return resolve({
+          solution,
+          dictionary,
+          possible: dictionary
+            .filter(word => hasAllPresent(summary, word))
+            .filter(word => regex.test(word))
+        });
+      });
+    }).then(({dictionary, possible, solution}) => {
+      const possibleUsage = getUsage(possible);
+      const fullUsage = getUsage(dictionary);
+
+      return possible
+        .sort((a, b) => midSort({usage: fullUsage, length: dictionary.length, a, b }))
+        .sort((a, b) => midSort({usage: possibleUsage, length: possible.length, a, b }))
+        .sort((a, b) => String(Array.from(new Set(b.split(''))).length).localeCompare(String(Array.from(new Set(a.split(''))).length), 'en', { numeric: true }))
+      ;
     });
-  },
+  };
 
-  solution: {
-    get: () => {
-      if (hinter.state.solution) {
-        return Promise.resolve(hinter.state.solution);
-      }
+  const getSiteName = () => {
+    let sites = {
+      wordle: /nytimes\.com\/games\/wordle\b/i
+    };
+    return Object.entries(sites)
+      .find(([key, value]) => value.test(document.location))
+      ?.[0];
+  };
 
-      let solutionMapper = {
-        default: () => Promise.reject('fail'),
-        wordle: () => {
-          return fetch(`/svc/wordle/v2/${new Date().toLocaleDateString('en-CA')}.json`)
-            .then(response => response.json())
-            .then(jsonData => {
-                hinter.state.solution = jsonData?.solution;
-                return jsonData?.solution;
-            })
-        }
-      };
+  const getDictionary = (site) => {
+    let dictionaryMapper = {
+      wordle: async() => fetch('.').then(resp => resp.text())
+        .then(data => data.match(/src=['"](.{0,100}wordle\.[\d\w]{15,25}\.js)/mi)?.pop())
+        .then((jsUrl) => fetch(jsUrl).then(resp => resp.text()))
+        .then(data => data.match(/(["'](\w{5})["'],? ?){100,}/g).shift().split(/['", ]+/).filter(Boolean))
+    };
+    return dictionaryMapper[site]();
+  };
 
-      return (solutionMapper[hinter.state.game] || solutionMapper.default)()
-        .then(word => {
-          console.log(`Solution was found: ${word}`);
-          return word;
-        });
-    }
-  },
-  words: {
-    get: () => {
-      if (hinter.state.dict) {
-        return Promise.resolve(hinter.state.dict);
-      }
+  const getBoard = (site) => {
+    let boardMapper = {
+      wordle: () => Array.from(document.getElementById('wordle-app-game').querySelectorAll('[class^="Row"], [class^="row"]'))
+        .map(row => Array.from(row.childNodes).map((tileNode) => getTile(site, tileNode)))
+    };
+    return boardMapper[site]();
+  };
 
-      let dictMapper = {
-        default: () => Promise.reject('fail'),
-        wordle: () => {
-          return fetch('./index.html')
-            .then(response => response.text())
-            .then(htmlData => {
-              let sourceFile = htmlData.match(/wordle\.[\w\d]+\.js/i)?.[0];
-              return fetch(`../../games-assets/v2/${sourceFile}`)
-                .then(response => response.text())
-                .then(jsData => {
-                  hinter.state.dict = JSON.parse(jsData.match(/SET_INITIAL_STATE.*?(\[.*?\])/i)[1]);
-                  return hinter.state.dict;
-                });
-            })
-        }
-      };
+  const getSolution = (site) => {
+    let solutionMapper = {
+      wordle: () => fetch(`https://www.nytimes.com/svc/wordle/v2/${(new Date()).toJSON().split('T').shift()}.json`).then(resp => resp.json())
+        .then(data => data.solution)
+    };
+    return solutionMapper[site]();
+  };
 
-      return (dictMapper[hinter.state.game] || dictMapper.default)()
-        .then(dict => {
-          console.log(`Dictionary with ${dict && dict.length} words obtained.`);
-          return dict;
-        });
-    },
-    hint: (dict, rows) => {
-      const {possibilities, exactArr} = hinter.words.possible(dict, rows);
-      let commonSort = hinter.words.sort.common(dict, possibilities, exactArr);
-      let output = [
-        `Possibilities: ${possibilities.length}`,
-        commonSort
-      ];
-      console.log(output);
+  const getTile = (site, tileNode) => {
+    return {
+      state: getTileState(site, tileNode),
+      letter: getTileLetter(site, tileNode),
+    };
+  };
 
-      alert(output.map(item => {
-        return typeof item === 'string'
-          ? item
-          : item.slice(0,10).map(word => `${word.weightedScore}: ${word.word}`).join('\n') + '\n';
-      }).join('\n').trim());
-    },
-    possible: (dict, rows) => {
-      let wordLength = rows[0].tiles.length;
-      let playedRows = rows.filter(row => row.status === 'played');
+  const getTileState = (site, tileNode) => {
+    let tileMapper = {
+      /* These match with no mapping */
+      wordle: () => tileNode.firstChild?.dataset?.state.toLowerCase()
+    };
+    return tileMapper[site]();
+  };
 
-      let exactArr = Array(wordLength).fill('');
-      let excludeArr = Array(wordLength).fill('');
-      let cards = ((output) => {
-        playedRows.forEach((row, i, array, rowOutput = {}) => {
-          row.tiles.forEach((tile, tileIndex) => {
-            rowOutput[tile.letter] = (rowOutput[tile.letter] ?? 0) +
-              (tile.statusId >= 3 ? 1 : 0);
+  const getTileLetter = (site, tileNode) => {
+    let tileMapper = {
+      wordle: () => tileNode.innerText.toLowerCase()
+    };
+    return tileMapper[site]();
+  };
 
-            if (tile.status === 'correct') {
-              exactArr[tileIndex] = tile.letter;
-            }
+  const getBoardSummary = (board) => {
+    return {
+      absent: Array.from(new Set(
+        board
+          .flat()
+          .flat()
+          .filter(cell => cell.state === ABSENT)
+          .map(cell => cell.letter)
+      )),
+      present: Array.from(new Set(
+        board
+          .flat()
+          .flat()
+          .filter(cell => cell.state === PRESENT || cell.state === CORRECT)
+          .map(cell => cell.letter)
+      )),
+      exactPresent: Array.from(new Set(Array(board[0].length).fill('')
+        .map((nil, tileIndex) => board.map(row => row[tileIndex])
+          .filter(tile => tile.state === PRESENT)
+          .map(tile => tile.letter)
+        )))
+      ,
+      correct: Array(board[0].length).fill('')
+        .map((nil, tileIndex) => board.map(row => row[tileIndex])
+          .find(tile => tile.state === CORRECT)?.letter
+        )
+    };
+  };
 
-            if (tile.status === 'present') {
-              excludeArr[tileIndex] += tile.letter;
-            }
-          });
-          Object.keys(rowOutput).forEach(letter => {
-            output[letter] = Math.max(
-              (output[letter] ?? 0),
-              rowOutput[letter]
-            );
-          });
-        });
-        return output;
-      })({});
+  const getRegex = (summary) => {
+    return new RegExp(Array(summary.correct.length).fill('')
+      .map((nil, tileIndex) => summary.correct[tileIndex] ??
+        `[^${summary.absent.join('')}${summary.exactPresent[tileIndex].join('')}]`
+      ).join(''), 'i');
+  };
 
-      let exactReg = new RegExp(`^${exactArr.map(letter => letter || '\\w').join('')}$`, 'i');
-      let excludeReg = new RegExp(`^${excludeArr.map(letter => letter ? `[^${letter}]` : '\\w').join('')}$`, 'i');
-      return {
-        possibilities: dict
-          .filter(word => word.match(exactReg))
-          .filter(word => word.match(excludeReg))
-          .filter(word => Object.keys(cards).every(letter => {
-            return cards[letter] // if there are *some* of this letter
-              ? (word.match(new RegExp(letter, 'gi'))?.length ?? 0) >= cards[letter]
-              : !word.match(letter);
-          })),
-        exactArr
-      };
-    },
-    sort: {
-      common: (dict, possibilities, exactArr) => {
-        const [possibleDistribution, allDistribution] = [possibilities, dict]
-          .map(words => {
-            return words.length && Array(words[0].length)
-              .fill('').map(() => { return {}; })
-              .map((slot, i) => {
-                possibilities.forEach(possibility => {
-                  slot[possibility[i]] = (slot[possibility[i]] ?? 0) + 1;
-                })
-                return slot;
-              });
-          });
+  const hasAllPresent = (summary, word) => summary.present?.every(letter => word.toLowerCase().includes(letter));
 
-        const weightedDistribution = JSON.parse(JSON.stringify(possibleDistribution)).map((column, i) => {
-          let letter = hinter.state.solution[i];
-          return {
-            ...column,
-            [letter]: column[letter]
-              * hinter.state.solution.match(new RegExp(letter, 'g')).length
-              * 1.1
-          };
-        });
-
-        let score = (distribution, word) => {
-          return Number((word.split('').reduce((accumulator, letter, i) => {
-            let distributionScore = (distribution?.[i]?.[letter] ?? 0);
-            let letterCount = word.match(new RegExp(letter,'g')).length;
-
-            return accumulator +
-              (letter === exactArr[i]
-                ? 0
-                : (distributionScore / letterCount)
-              );
-          }, 0)).toPrecision(2));
-        };
-
-        return possibilities
-          .map(word => {
-            return {
-              word,
-              weightedScore: score(weightedDistribution, word),
-              possibleScore: score(possibleDistribution, word),
-              // allScore: score(allDistribution, word)
-            };
-          })
-          // .sort((a, b) => String(b.allScore).localeCompare(String(a.allScore), 'en', { numeric: true }))
-          .sort((a, b) => String(b.possibleScore).localeCompare(String(a.possibleScore), 'en', { numeric: true }))
-          .sort((a, b) => String(b.weightedScore).localeCompare(String(a.weightedScore), 'en', { numeric: true }))
-      }
-    }
-  },
-
-  util: {
-    resolveMapper: (mapper, ...args) => {
-      if (mapper.hasOwnProperty(hinter.state.game)) {
-        return mapper[hinter.state.game](...args);
-      }
-
-      let keys = Object.keys(mapper);
-      for (let i = 0; i < keys.length; i++) {
-        let output = mapper[keys[i]](...args);
-        if (!isNaN(output) || output) {
-          return output;
-        }
-      }
-    },
-  },
-
-  rows: {
-    getDom: () => {
-      return hinter.util.resolveMapper({
-        wordle: () => {
-          return Array.from(document.querySelectorAll('[role="group"]'))
-            .filter(node => node.className.match('row'));
-        }
+  const getUsage = (dictionary) => {
+    return Array(dictionary[0].length).fill('')
+      .map((nil, tileIndex) => {
+        return dictionary.reduce((arr, word) => Object.assign(arr, {
+          [word[tileIndex]]: (arr[[word[tileIndex]]] ?? 0) + 1
+        }), {});
       });
-    },
-    status: (statusRow) => {
-      return hinter.util.resolveMapper({
-        wordle: (wordleRow) => {
-          return [
-            (row) => !Boolean(row.node.innerText),
-            (row) => row.node.querySelector('[data-state="empty"]'),
-            () => { return true; }
-          ].findIndex(mapper => Boolean(mapper(wordleRow)));
-        }
-      }, statusRow);
-    }
-  },
-  tiles: {
-    getDom: () => {
-      return hinter.util.resolveMapper({
-        wordle: () => {
-          return hinter.rows.getDom()
-            ?.map(rowNode => {
-              return {
-                node: rowNode,
-                tiles: Array.from(rowNode.children).map(tile => {
-                  return {
-                    node: tile
-                  };
-                })
-              };
-            });
-        }
-      });
-    },
-    status: (statusTile) => {
-      return hinter.util.resolveMapper({
-        wordle: (wordleTile) => {
-          let tileState = wordleTile.node.querySelector('[data-state]').dataset.state;
-          return [
-            'empty',
-            'tbd',
-            'absent',
-            'present',
-            'correct',
-          ].indexOf(tileState);
-        }
-      }, statusTile);
-    },
-    letter: (statusTile) => {
-      return hinter.util.resolveMapper({
-        wordle: (wordleTile) => {
-          return wordleTile.node.innerText.trim();
-        }
-      }, statusTile);
-    }
-  },
-};
+  };
 
+  const midSort = ({ usage, length, a, b }) => {
+    const [aScore, bScore] = [a, b].map(word => word.toLowerCase().split('').reduce((aggregate, letter, index) => {
+      return aggregate + Math.abs(length/2 - usage[index][letter]);
+    }, 0));
+    return String(aScore).localeCompare(String(bScore), 'en', { numeric: true });
+  };
 
-hinter.init();
+  /* INITIALIZATION */
+  return init().catch(err => {
+    alert(err?.message ?? 'Unknown error');
+  }).then(possible => {
+    alert('Possible Options:\n' +
+      possible.slice(0, 10).map(word => `* ${word}`).join('\n')
+    );
+  });
